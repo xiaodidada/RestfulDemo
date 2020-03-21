d856a2567c4deec7177db5b2e93dba58f7d12505
xiaodidada <2565960363@qq.com>
2020-03-21 17:38:25 +0800

    初建项目

---
+++ RestfulDemo/src/main/java/com/syzc/RestfulDemo/RestfulDemoApplication.java
@@ -0,0 +1,13 @@
+package com.syzc.RestfulDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulDemoApplication.class, args);
+	}
+
+}
